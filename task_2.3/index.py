4bf8a13bf9684a565500e3782e5b40d6cc9c656d
Dmitry Putilov <Dmit.Putilov@x5.ru>
2024-11-06 13:56:00 +0700

    Add task 2.3

---
+++ task_2.3/index.py
@@ -0,0 +1,20 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('BOT-TOKEN')
+
+@bot.message_handler(commands = ['start'])
+def url(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text='Наш сайт', url='https://habr.com/ru/all/')
+    markup.add(btn1)
+    bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на сайт хабра", reply_markup = markup)
+
+    @bot.message_handler(commands=['start'])
+def start(message):
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("🇷🇺 Русский")
+    btn2 = types.KeyboardButton('🇬🇧 English')
+    markup.add(btn1, btn2)
+    bot.send_message(message.from_user.id, "🇷🇺 Выберите язык / 🇬🇧 Choose your language", reply_markup=markup)
